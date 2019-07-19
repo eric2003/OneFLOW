3401a7a9ed5c7c9cee3f12e6cd8657d6209870d1
eric <fantasy_2003_@hotmail.com>
2019-07-19 17:03:52 +0800

    Update ResidualTask.cpp

--- codes/residual/src/ResidualTask.cpp
+++ codes/residual/src/ResidualTask.cpp
@@ -131,28 +131,37 @@ void ResidualTask::CmpRes( int sTid, ResData & data )
 
 void ResidualTask::PostDumpResiduals()
 {
+    cout << "PostDumpResiduals 1 \n";
     size_t nEqu = this->data.resave.res.size();
-
+    cout << "PostDumpResiduals 2 \n";
     this->data.resave.CmpAver( dataList );
     this->data.resmax.CmpMax( dataList );
+    cout << "PostDumpResiduals 3 \n";
 
     if ( Parallel::pid != Parallel::serverid ) return;
+    cout << "PostDumpResiduals 4 \n";
 
     this->DumpScreen();
+    cout << "PostDumpResiduals 5 \n";
     this->DumpFile();
+    cout << "PostDumpResiduals 6 \n";
 }
 
 void ResidualTask::DumpFile()
 {
+    cout << "DumpFile 1 \n";
     ostringstream oss;
 
     fstream file;
     SolverInfo * solverInfo = SolverInfoFactory::GetSolverInfo( SolverState::tid );
     string & fileName = solverInfo->resFileName;
+    cout << "DumpFile 2 \n";
     PIO::ParallelOpenPrj( file, fileName, ios_base::out | ios_base::app );
+    cout << "DumpFile 3 \n";
 
     if ( IsEmpty( file ) )
     {
+        cout << "DumpFile 4 \n";
         StringField title;
         title.push_back( "Title=\"THE RESIDUAL OF ONEFLOW\"" );
         title.push_back( "Variables=" );
@@ -163,12 +172,15 @@ void ResidualTask::DumpFile()
         {
             title.push_back( AddString( "\"res",  iVar + 1, "\"" ) );
         }
+        cout << "DumpFile 5 \n";
 
         for ( UInt iTitle = 0; iTitle < title.size(); ++ iTitle )
         {
             oss << title[ iTitle ] << endl;
         }
+        cout << "DumpFile 6 \n";
     }
+    cout << "DumpFile 7 \n";
 
     oss << setiosflags( ios::left );
     oss << setprecision( 5 );
@@ -178,22 +190,33 @@ void ResidualTask::DumpFile()
     oss << Iteration::outerSteps << " ";
     oss << Iteration::innerSteps << " ";
 
+    cout << "DumpFile 8 \n";
+
     size_t nVar = this->data.resave.res.size();
     for ( int iVar = 0; iVar < nVar; ++ iVar )
     {
         oss << setw( 13 ) << this->data.resave.res[ iVar ] << " ";
     }
 
+    cout << "DumpFile 9 \n";
+
     oss << endl;
 
     file << oss.str();
 
+    cout << "DumpFile 10 \n";
+
     PIO::ParallelClose( file );
+
+    cout << "DumpFile 11 \n";
 }
 
 void ResidualTask::DumpScreen()
 {
+    cout << "DumpScreen 1 \n";
     int maxId = this->data.resmax.CmpMaxId();
+
+    cout << "DumpScreen 2 \n";
 
     ostringstream oss;
     if ( ( Iteration::outerSteps - 1 ) % 100 == 0 )
@@ -201,7 +224,7 @@ void ResidualTask::DumpScreen()
         oss << endl;
         oss << "iter initer ave  max zone cell vol  nv \n";
     }
-
+    cout << "DumpScreen 3 \n";
     oss << setiosflags( ios::left );
     oss << setprecision( 5 );
     oss << setiosflags( ios::scientific );
@@ -221,8 +244,10 @@ void ResidualTask::DumpScreen()
     oss << " ";
     oss << setw( 3 )  << maxId + 1;
     oss << endl;
+    cout << "DumpScreen 4 \n";
 
     cout << oss.str();
+    cout << "DumpScreen 5 \n";
 }
 
 
