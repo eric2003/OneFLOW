27cd26050a6c0821982ef288a8a0eed508fe23fc
eric <fantasy_2003_@hotmail.com>
2019-07-25 21:38:49 +0800

    Update installHDF5.ps1

--- ci/appveyor/installHDF5.ps1
+++ ci/appveyor/installHDF5.ps1
@@ -8,6 +8,15 @@ function InstallHDF5() {
     ls
     Start-Process "C:/Program Files/7-zip/7z.exe" -Wait -ArgumentList 'x .\hdf5-1.10.5.tar'
     ls
+    cd hdf5-1.10.5
+    md build
+    $env:Path += ";C:/Program Files/CMake/bin/"
+    cmake --version
+    cmake -G "Visual Studio 14 2015 Win64" -SC:/projects/OneFLOW/lib/hdf5-1.10.5 -BC:/projects/OneFLOW/lib/hdf5-1.10.5/build/ `
+    -DBUILD_SHARED_LIBS="OFF" -DCMAKE_INSTALL_PREFIX=C:/Program Files/HDF_Group/HDF5/1.10.5 ..
+    Write-Host "cmake -G finished"
+    cmake --build . --target INSTALL --config release
+    Write-Host "cmake --build --target INSTALL --config release finished"
     Write-Host "Installing HDF5 Finished..."
 }
 
