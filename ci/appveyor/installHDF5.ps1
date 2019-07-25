07f8815dcc7e85e651ea11a2a016e6312af87265
eric <fantasy_2003_@hotmail.com>
2019-07-25 21:19:28 +0800

    InstallHDF5

--- ci/appveyor/installHDF5.ps1
+++ ci/appveyor/installHDF5.ps1
@@ -1,7 +1,13 @@
 function InstallHDF5() {
-    md HDF5_DIR
-    cd HDF5_DIR
     Write-Host "Installing HDF5..."
+    cd lib
+    ls
+    Write-Host "ls C:/Program Files/7-zip"
+    ls "C:/Program Files/7-zip"
+    Start-Process "C:/Program Files/7-zip/7z.exe" -Wait -ArgumentList 'x .\hdf5-1.10.5.tar.gz'
+    ls
+    Start-Process "C:/Program Files/7-zip/7z.exe" -Wait -ArgumentList 'x .\hdf5-1.10.5.tar'
+    ls
     Write-Host "Installing HDF5 Finished..."
 }
 
