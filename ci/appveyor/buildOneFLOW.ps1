fc56b6b4ccb579f37c672ee5866f289f844e0879
eric <fantasy_2003_@hotmail.com>
2019-07-25 16:12:05 +0800

    Update buildOneFLOW.ps1

--- ci/appveyor/buildOneFLOW.ps1
+++ ci/appveyor/buildOneFLOW.ps1
@@ -3,6 +3,8 @@ function BuildOneFLOW() {
     # Build OneFLOW with cmake
     md build
     cd build
+    $env:Path += ";C:/Program Files/CMake/bin/"
+    cmake --version
     cmake -G "Visual Studio 14 2015 Win64"
     cmake --build . --target INSTALL --config release
 }
