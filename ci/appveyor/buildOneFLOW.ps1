d7f4d27b68d61e2d359cba6d303ab7ed68bf3e66
eric <fantasy_2003_@hotmail.com>
2019-07-26 01:57:54 +0800

    Update buildOneFLOW.ps1

--- ci/appveyor/buildOneFLOW.ps1
+++ ci/appveyor/buildOneFLOW.ps1
@@ -7,8 +7,8 @@ function BuildOneFLOW() {
     cmake --version
     Write-Host "cmake --help..."
     cmake --help
-    cmake -G "Visual Studio 14 2015 Win64" -SC:/projects/OneFLOW/ -BC:/projects/OneFLOW/build/ `
-    -DAUTO_CI_TEST=ON
+    cmake -G "Visual Studio 14 2015 Win64" -SC:/projects/OneFLOW -BC:/projects/OneFLOW/build/ `
+    -DAUTO_CI_TEST=ON -DCMAKE_INSTALL_PREFIX="C:/Program Files/OneFLOW"
     Write-Host "cmake -G finished..."
     cmake --build . --target INSTALL --config release
     Write-Host "  cmake --build . --target INSTALL --config release..."
