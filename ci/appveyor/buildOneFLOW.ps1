f7816288ad6e65f0e0ea44c09377a5c0c268f2b3
eric <fantasy_2003_@hotmail.com>
2019-07-25 17:18:03 +0800

    auto test

--- ci/appveyor/buildOneFLOW.ps1
+++ ci/appveyor/buildOneFLOW.ps1
@@ -8,6 +8,7 @@ function BuildOneFLOW() {
     Write-Host "cmake --help..."
     cmake --help
     cmake -G "Visual Studio 14 2015 Win64" -SC:/projects/OneFLOW/ -BC:/projects/OneFLOW/build/
+    -DAUTO_CI_TEST=ON
     cmake --build . --target INSTALL --config release
 }
 
