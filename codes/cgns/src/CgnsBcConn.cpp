378e5ddfe529088d5cd782aea1cccdaeb3454436
eric <fantasy_2003_@hotmail.com>
2020-03-02 16:44:54 +0800

    add CgnsBc1to1

---
+++ codes/cgns/src/CgnsBcConn.cpp
@@ -0,0 +1,50 @@
+/*---------------------------------------------------------------------------*\
+OneFLOW - LargeScale Multiphysics Scientific Simulation Environment
+Copyright (C) 2017-2020 He Xin and the OneFLOW contributors.
+-------------------------------------------------------------------------------
+License
+This file is part of OneFLOW.
+
+OneFLOW is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OneFLOW is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with OneFLOW.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "CgnsBcConn.h"
+#include "CgnsBcRegion.h"
+#include "CgnsZone.h"
+#include "CgnsBase.h"
+#include "CgnsPeriod.h"
+#include "CgnsGlobal.h"
+#include "NodeMesh.h"
+#include "HXMath.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+BeginNameSpace( ONEFLOW )
+
+#ifdef ENABLE_CGNS
+
+CgnsBcConn::CgnsBcConn( CgnsBcRegion * bcRegion )
+{
+}
+
+CgnsBcConn::~CgnsBcConn()
+{
+    ;
+}
+
+#endif
+
+EndNameSpace
