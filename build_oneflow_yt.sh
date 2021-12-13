ea27ff7be7fb0c50225d7ec8f173e66fccab5e5b
QILIN LU <luqilinheu@gmail.com>
2021-12-13 15:49:16 +0800

    Add yantai acc script

--- build_oneflow_yt.sh
+++ build_oneflow_yt.sh
@@ -1,14 +1,15 @@
 prj_dir=${PWD}
+mkdir build
 cd build
 rm -rf *
 module avail
 module purge
 module load gnu8/8.3.0
-module load nvhpc/21.9
+module load nvhpc/21.7
 module load cmake/gcc-8.3.0/3.21.1
-module load metis/nvhpc-21.9/5.1.0
-module load hdf5/nvhpc-21.9/1.8.22
-module load cgns/nvhpc-21.9/4.2.0
+module load metis/nvhpc-21.7/5.1.0
+module load hdf5/nvhpc-21.7/1.8.22
+module load cgns/nvhpc-21.7/4.2.0
 pwd
 root_dir=${PWD}
 root_dir=${PWD}
