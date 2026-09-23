3b9b8c19c89f380e5a947bcd08cb6a6b00da4aab
eric <fantasy_2003_@hotmail.com>
2026-09-24 03:24:25 +0800

    update

--- codes/uns/src/UNsUnsteady.cpp
+++ codes/uns/src/UNsUnsteady.cpp
@@ -75,9 +75,14 @@ void UNsUnstPrepareSrcData( Unsteady * unst )
     UsdField * field = unst->field;
     for ( int iEqu = 0; iEqu < data->nEqu; ++ iEqu )
     {
-        data->prim [ iEqu ] = ( * field->q  )[ iEqu ][ ug.cId ];
-        data->prim1[ iEqu ] = ( * field->q1 )[ iEqu ][ ug.cId ];
-        data->prim2[ iEqu ] = ( * field->q2 )[ iEqu ][ ug.cId ];
+        data->prim [ iEqu ] =
+            ( * field->flow[ 0 ] )[ iEqu ][ ug.cId ];
+
+        data->prim1[ iEqu ] =
+            ( * field->flow[ 1 ] )[ iEqu ][ ug.cId ];
+
+        data->prim2[ iEqu ] =
+            ( * field->flow[ 2 ] )[ iEqu ][ ug.cId ];
     }
     nscom.gama = ( * unsf.gama  )[ 0 ][ ug.cId ];
     gcom.cvol  = ( * ug.cvol  )[ ug.cId ];
