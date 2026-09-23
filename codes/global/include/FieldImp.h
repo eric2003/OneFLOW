7f1de76de0aed3657ed113d3653fec1181aaf005
eric <fantasy_2003_@hotmail.com>
2026-09-24 00:41:01 +0800

    Update FieldImp.h

--- codes/global/include/FieldImp.h
+++ codes/global/include/FieldImp.h
@@ -112,23 +112,6 @@ public:
         const std::string & fieldName,
         Real value );
 
-    //void AllocateGridFields();
-
-    //void AllocateGridFields(
-    //    UnsGrid * grid,
-    //    FieldPropertyData * fieldPropertyData );
-
-    //void AllocateInnerField(
-    //    UnsGrid * grid,
-    //    FieldPropertyData * fieldPropertyData );
-
-    //void AllocateFaceField(
-    //    UnsGrid * grid,
-    //    FieldPropertyData * fieldPropertyData );
-
-    //void AllocateBcField(
-    //    UnsGrid * grid,
-    //    FieldPropertyData * fieldPropertyData );
 private:
     FieldPropertyData commManager;
     FieldPropertyData strManager;
