df2e8a3e60298d888d02dcdc5f0f73939551e7c2
eric <fantasy_2003_@hotmail.com>
2026-09-26 06:10:27 +0800

    update

---
+++ codes/global/include/FieldAllocConfig.h
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*\
+OneFLOW - LargeScale Multiphysics Scientific Simulation Environment
+Copyright (C) 2017-2026 He Xin and the OneFLOW contributors.
+-------------------------------------------------------------------------------
+License
+This file is part of OneFLOW.
+
+OneFLOW is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OneFLOW is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with OneFLOW.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+#pragma once
+#include "NamespaceMacros.h"
+#include "HXDefine.h"
+#include <string>
+
+BeginNameSpace( ONEFLOW )
+
+class TextFileParser;
+
+// Parse integer token: digit literal, or DataBase variable name.
+int ResolveIntegerValue( const std::string & valueToken );
+
+// Field name list from inter*.txt style "true, name;" lines.
+class FieldNameList
+{
+public:
+    void Add( const std::string & name );
+    int Size() const;
+    const std::string & GetName( int index ) const;
+
+private:
+    StringField nameList;
+};
+
+// Name/value pairs from init.txt style lines.
+class NameValuePair
+{
+public:
+    void Add( const std::string & name, Real value );
+    int Size() const;
+    const std::string & GetName( int index ) const;
+    Real GetValue( int index ) const;
+
+private:
+    StringField nameList;
+    RealField valueList;
+};
+
+// Reader for system/<solver>/alloc/ text:
+//   inter*.txt  -> ReadFile()      -> GetFieldNameList()
+//   init.txt    -> ReadValueFile() -> GetNameValuePair()
+// Supports true / bool / superbool control lines.
+class FieldConfigReader
+{
+public:
+    const FieldNameList & GetFieldNameList() const;
+    const NameValuePair & GetNameValuePair() const;
+
+    bool GetBoolValue( const std::string & varName ) const;
+    void Add( const std::string & name, bool value );
+
+    void ReadBool( TextFileParser & textFileParser );
+    void ReadSuperBool( TextFileParser & textFileParser );
+    void ReadName( TextFileParser & textFileParser );
+    void ReadNameValue( TextFileParser & textFileParser );
+
+    void ReadFile( const std::string & fileName );
+    void ReadValueFile( const std::string & fileName );
+
+private:
+    StringField boolNameList;
+    BoolField boolValueList;
+    FieldNameList fieldNameList;
+    NameValuePair nameValuePair;
+};
+
+EndNameSpace
